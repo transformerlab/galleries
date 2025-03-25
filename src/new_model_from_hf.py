30f85a0b3ae35f3532e9f4afb110e1ade0654bb3
Tony Salomone <dadmobile@gmail.com>
2025-03-25 13:24:31 -0400

    Populate the logo field on new models

--- src/new_model_from_hf.py
+++ src/new_model_from_hf.py
@@ -6,41 +6,54 @@ import argparse
 
 # take in one paramter called model_id as an argument:
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_id', type=str,
-                    help='The model id in huggingface e.g. Qwen/Qwen2-VL-7B-Instruct you can also pass a group of models, comma separated', required=True)
+parser.add_argument(
+    "--model_id",
+    type=str,
+    help="The model id in huggingface e.g. Qwen/Qwen2-VL-7B-Instruct you can also pass a group of models, comma separated",
+    required=True,
+)
 # add a paramter that is the output filename you want:
-parser.add_argument('--output_file_name', type=str,
-                    help='The short output file name e.g. "qwen"', required=True)
-parser.add_argument('--format', type=str,required=False, default='yaml',
-                    help='The format of the output file e.g. "yaml" or "json"')
+parser.add_argument(
+    "--output_file_name",
+    type=str,
+    help='The short output file name e.g. "qwen"',
+    required=True,
+)
+parser.add_argument(
+    "--format",
+    type=str,
+    required=False,
+    default="yaml",
+    help='The format of the output file e.g. "yaml" or "json"',
+)
 args = parser.parse_args()
 model_id = args.model_id
 output_file_name = args.output_file_name
 format = args.format
 
 # check if model_id is an array of model ids separated by commas:
-if ',' in model_id:
-    models = model_id.split(',')
-    # 
+if "," in model_id:
+    models = model_id.split(",")
+    #
 else:
     models = [model_id]
 
-model_objects = []  
+model_objects = []
 
 for model_id in models:
     # verify it is a valid model id
-    if '/' not in model_id:
+    if "/" not in model_id:
         print("Invalid model id")
         pass
 
     print("-----------MODEL ID")
     print(model_id)
     # end part of model name is the part after the / in the model name
-    model_author = model_id.split('/')[0]
-    model_name = model_id.split('/')[1]
+    model_author = model_id.split("/")[0]
+    model_name = model_id.split("/")[1]
 
-    card = hf_hub_download(model_id, 'README.md')
-    config = hf_hub_download(model_id, 'config.json')
+    card = hf_hub_download(model_id, "README.md")
+    config = hf_hub_download(model_id, "config.json")
 
     # card is the README.md file, open it and print to screen:
     # with open(card, 'r') as file:
@@ -48,30 +61,32 @@ for model_id in models:
 
     print("-----------MODEL CARD")
     config_json = None
-    with open(config, 'r') as file:
+    with open(config, "r") as file:
         config_json = json.load(file)
         print(config_json)
 
     readme = None
-    with open(card, 'r') as file:
+    with open(card, "r") as file:
         readme = file.read()
 
     print("-----------MODEL DETAILS")
-    architecture = config_json['architectures'][0]
+    architecture = config_json["architectures"][0]
     print(architecture)
-    context_length = config_json.get('max_position_embeddings', "2048")
+    context_length = config_json.get("max_position_embeddings", "2048")
     context_length = str(context_length)
     print(context_length)
-    transformers_version = config_json['transformers_version']
+    transformers_version = config_json["transformers_version"]
     print(transformers_version)
 
     # gated information is USUALLY stored in config
     # But newer models seem to not set this and then set a custom
     # gated prompt on the model card
-    gated = config_json.get('gated', False)
+    gated = config_json.get("gated", False)
     if not gated:
-        for line in readme.split('\n'):
-            if line.startswith('extra_gated_prompt:') or line.startswith('extra_gated_fields:'):
+        for line in readme.split("\n"):
+            if line.startswith("extra_gated_prompt:") or line.startswith(
+                "extra_gated_fields:"
+            ):
                 gated = "manual"
                 break
     print(gated)
@@ -81,11 +96,42 @@ for model_id in models:
     # license: apache-2.0
     # and get the value of the second part:
     license = None
-    for line in readme.split('\n'):
-        if line.startswith('license:'):
-            license = line.split('license:')[1].strip()
+    for line in readme.split("\n"):
+        if line.startswith("license:"):
+            license = line.split("license:")[1].strip()
             print(license)
             break
+
+    # Try to figure out logo based on model info
+    lc_model_name = model_name.lower()
+    lc_architecture = architecture.lower()
+    logo = ""
+    if "cohere" in lc_architecture:
+        logo = "https://avatars.githubusercontent.com/u/54850923?s=200&v=4"
+    elif "deepseek" in lc_model_name:
+        logo = "https://www.deepseek.com/favicon.ico"
+    elif "gemma" in lc_architecture:
+        logo = "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/gemma-header.width-1200.format-webp.webp"
+    elif "mistral" in lc_architecture:
+        logo = "https://huggingface.co/mistralai/Mistral-Small-3.1-24B-Instruct-2503"
+    elif "qwen" in lc_architecture:
+        logo = "https://cdn-avatars.huggingface.co/v1/production/uploads/62088594a5943c8a8fc94560/y5SEKiE8TkjBKs9xfjCx5.png"
+    elif "smol" in lc_model_name:
+        logo = "https://webml-community-smollm-webgpu.static.hf.space/logo.png"
+    elif "vicuna" in lc_model_name:
+        logo = "https://lmsys.org/images/blog/vicuna/vicuna.jpeg"
+
+    # Do these later because I coulddn't figure out a good check without false positives
+    elif "phi-" in lc_model_name:
+        logo = "https://blogs.microsoft.com/wp-content/uploads/prod/2012/08/8867.Microsoft_5F00_Logo_2D00_for_2D00_screen.jpg"
+    elif "llama-" in lc_model_name:
+        logo = "https://upload.wikimedia.org/wikipedia/commons/a/ab/Meta-Logo.png"
+
+    # Do these two last as we want to capture the base architecture first if possible
+    elif "mlx" in architecture:
+        logo = "https://ml-explore.github.io/mlx/build/html/_static/mlx_logo.png"
+    elif "gguf" in architecture:
+        logo = "https://user-images.githubusercontent.com/229941/224846830-71002ab8-6c0d-49bc-bfcc-669618bbcdbe.png"
 
     # Now we want to make a file that looks like:
     # uniqueID: "HuggingFaceH4/zephyr-7b-alpha"
@@ -114,30 +160,30 @@ for model_id in models:
     # and write them to a new file
 
     model_object = {}
-    model_object['uniqueID'] = model_id
-    model_object['name'] = model_name
-    model_object['description'] = ""
-    model_object['parameters'] = ""
-    model_object['context'] = context_length
-    model_object['architecture'] = architecture
-    model_object['formats'] = ["Safetensors"]
-    model_object['huggingface_repo'] = model_id
-    model_object['transformers_version'] = transformers_version
-    model_object['gated'] = gated
-    model_object['license'] = license
-    model_object['logo'] = ""
-    model_object['size_of_model_in_mb'] = 0
-    model_object['author'] = {}
-    model_object['author']['name'] = model_author
-    model_object['author']['url'] = f"https://huggingface.co/{model_id}"
-    model_object['author']['blurb'] = ""
-    model_object['resources'] = {}
-    model_object['resources']['canonicalUrl'] = f"https://huggingface.co/{model_id}"
-    model_object['resources']['downloadUrl'] = f"https://huggingface.co/{model_id}"
-    model_object['resources']['paperUrl'] = "?"
+    model_object["uniqueID"] = model_id
+    model_object["name"] = model_name
+    model_object["description"] = ""
+    model_object["parameters"] = ""
+    model_object["context"] = context_length
+    model_object["architecture"] = architecture
+    model_object["formats"] = ["Safetensors"]
+    model_object["huggingface_repo"] = model_id
+    model_object["transformers_version"] = transformers_version
+    model_object["gated"] = gated
+    model_object["license"] = license
+    model_object["logo"] = logo
+    model_object["size_of_model_in_mb"] = 0
+    model_object["author"] = {}
+    model_object["author"]["name"] = model_author
+    model_object["author"]["url"] = f"https://huggingface.co/{model_id}"
+    model_object["author"]["blurb"] = ""
+    model_object["resources"] = {}
+    model_object["resources"]["canonicalUrl"] = f"https://huggingface.co/{model_id}"
+    model_object["resources"]["downloadUrl"] = f"https://huggingface.co/{model_id}"
+    model_object["resources"]["paperUrl"] = "?"
 
     print("-----------FINAL MODEL OBJECT")
-    #print(yaml.dump(model_object, sort_keys=False))
+    # print(yaml.dump(model_object, sort_keys=False))
 
     model_objects.append(model_object)
 
@@ -152,8 +198,8 @@ if os.path.exists(file_name):
     append = True
 
 if append:
-    with open(file_name, 'r') as file:
-        if file_name.endswith('.yaml'):
+    with open(file_name, "r") as file:
+        if file_name.endswith(".yaml"):
             final_model_object = yaml.load(file, Loader=yaml.FullLoader)
         else:
             final_model_object = json.load(file)
@@ -161,7 +207,7 @@ if append:
         final_model_object.append(model_objects)
     else:
         final_model_object = [final_model_object, model_objects]
-else: 
+else:
     final_model_object = model_objects
 
 # if model_objects if of size 1, then we want to write it as a single object
@@ -170,12 +216,12 @@ if len(final_model_object) == 1:
 
 output = ""
 
-if format == 'json':
+if format == "json":
     print("writing as json")
     output = json.dumps(final_model_object, indent=2, sort_keys=False)
 else:
     print("writing as yaml")
     output = yaml.dump(final_model_object, sort_keys=False)
 
-with open(file_name, 'w+') as file:
+with open(file_name, "w+") as file:
     file.write(output)
